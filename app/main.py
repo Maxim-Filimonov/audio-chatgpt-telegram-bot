9213c3fbc27e207c4b5f74934bb9fb7fcf808b2e
tpaktop <tpaktopsp@gmail.com>
2023-03-09 09:57:28 +0000

    remove chatgpt response - add handling of file attachments

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
-import logging
-import logging.config
-import os
-import subprocess
-import time
-import telegram
-from dotenv import load_dotenv
-import openai
-import sys
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import sys
+import openai
+from dotenv import load_dotenv
+import telegram
+import time
+import subprocess
+import os
+import logging.config
+import logging
 from database.database import *
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 load_dotenv("app/.env")
 
@@ -28,15 +29,11 @@ def start_command_handler(update, context):
     add_new_user(str(update.message.chat.id))
 
     start_text = """
-Hi there, this bot allows you to query ChatGPT directly from Telegram, even with voice messages! 🤯
-It keeps track of your past messages, remember to select /reset from the command menu to cancel the history.
-    
-Code: https://github.com/faviasono/audio-chatgpt-telegram-bot
-Credits: @faviasono ✌🏻
-
+Hi there, this bot allows you to query Transcribe your voice or file attachments directly from Telegram
+Credits: @faviasono for original idea✌🏻
 You are ready to go 🚀
     """
-    
+
     update.message.reply_text(start_text)
 
 
@@ -51,7 +48,7 @@ def echo(update, context):
 def transcribe_voice_message(voice_message: str) -> str:
     """Transcribe voice message using Wishper model."""
     # Use the Whisper AI API to transcribe the voice message
-    audio_file= open(voice_message, "rb")
+    audio_file = open(voice_message, "rb")
     result = openai.Audio.transcribe("whisper-1", audio_file)
 
     return result["text"]
@@ -63,16 +60,32 @@ def handle_voice_message(update, context):
     voice_message = context.bot.get_file(update.message.voice.file_id)
     print(voice_message)
     voice_message.download("/tmp/audio.oga")
-    subprocess.run(["ffmpeg", "-y", "-i", '/tmp/audio.oga', '/tmp/audio.mp3'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["ffmpeg", "-y", "-i", '/tmp/audio.oga', '/tmp/audio.mp3'],
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     # Transcribe the voice message
     text = transcribe_voice_message("/tmp/audio.mp3")
 
     # Answer
     telegram_id = str(update.message.chat.id)
-    answer = generate_response(text, telegram_id)
     # Send the transcribed text back to the user
-    update.message.reply_text(answer)
+    update.message.reply_text(text)
+
+
+def handle_file_attachment(update, context):
+    file = context.bot.get_file(update.message.audio.file_id)
+    print(file)
+    logging.info("Handling file of type:" + update.message.audio.mime_type)
+    if update.message.audio.mime_type == "audio/mpeg":
+        file.download("/tmp/" + file.file_unique_id + ".mp3")
+        text = transcribe_voice_message("/tmp/" + file.file_unique_id + ".mp3")
+        update.message.reply_text(text)
+    if update.message.audio.mime_type == "audio/x-m4a":
+        file.download("/tmp/" + file.file_unique_id + ".m4a")
+        text = transcribe_voice_message("/tmp/" + file.file_unique_id + ".m4a")
+        update.message.reply_text(text)
+    else:
+        update.message.reply_text("Sorry, I can't handle this file type 😔")
 
 
 def generate_response(question: str, telegram_id: str) -> str:
@@ -81,7 +94,8 @@ def generate_response(question: str, telegram_id: str) -> str:
     row = retrieve_history(telegram_id)
     prompt = create_question_prompt(row, question)
 
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=prompt)
     answer = response["choices"][0]["message"]["content"]
 
     logging.info("Question: %s", question)
@@ -118,6 +132,7 @@ def main():
     # message handler
     dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_handler(MessageHandler(Filters.voice, handle_voice_message))
+    dp.add_handler(MessageHandler(Filters.attachment, handle_file_attachment))
 
     # log all errors
     dp.add_error_handler(error)
@@ -152,7 +167,6 @@ class DefaultConfig:
             format="%(asctime)s - %(levelname)s - %(message)s",
             level=DefaultConfig.LOG_LEVEL,
         )
-        
 
 
 if __name__ == "__main__":
